0b099df88c911142abaa53de9b47487f324d29cd
maximiliandannenberg <maximilian.dannenberg@st.hunimed.eu>
2024-05-25 00:20:19 +0200

    updated Chessmain.cpp

--- Chessmain.cpp
+++ Chessmain.cpp
@@ -11,6 +11,8 @@ States FEN is broken when finished with FENvalue and if we have the additional c
 New problem
 object is out of scope trying to run print function for board when function is out of the while loop.
 Create pointer for object to be referenced outside of the while loop?? - solution? 
+
+
 */
 
 
@@ -19,78 +21,41 @@ int main() {
 
 bool CheckFEN = false;
 while(CheckFEN == false){
+    std::string FEN;
+    std::cout << "please give the FEN to generate the desired board: \n";
+    std::getline(std::cin, FEN);
 
-std::string FEN;
+    FEN_Games game = CreateFEN(FEN);
 
-std::cout << "please give the FEN to generate the desired board: \n";
-std::getline(std::cin, FEN);
-
-FEN_Games game = CreateFEN(FEN);
-
-std::cout << game.FEN_valid << "\n";
-
-std::cout << game.FEN[1];
-
-if(game.FEN_valid == 1){
-    std::cout << "invalid FEN, please submit a working one. \n";
-    CheckFEN = false;
-} else {CheckFEN = true;}
-
-int rows = 8;
-int cols = 8;
-
-char** myboard = boardinitiliazer(rows, cols);
+    if(game.FEN_valid == 1){
+        std::cout << "invalid FEN, please submit a working one. \n";
+        CheckFEN = false;
+    } else {CheckFEN = true;}
 
 
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            int position = (i * 8) + j;
-            myboard[i][j] = game.FEN[position];
+
+    if(CheckFEN == true){
+    int rows = 8;
+    int cols = 8;
+
+    char** myboard = boardinitiliazer(rows, cols);
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                int position = (i * 8) + j;
+                myboard[i][j] = game.FEN[position];
+            }
         }
+
+   void printboard(char** myboard, int rows, int cols);
+    
     }
-
-for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            std::cout << myboard[i][j] << " ";
-        }
-        std::cout << '\n';
-    }
-
-
 }
 
+return 0;
 
-
-
-
-
-
-
-
-
-// std::vector<char> FEN_converted = FEN_converter(FEN);
-// // This function takes our fen string and converts it into a vector 
-// // This vector has 64 positions each representing each square on the board.
-
-
-
-
-
-//bool FEN_checked_pieces = FEN_Check_Piece_Count(FEN_converted); 
-//checks if their are the correct amount of pieces
-
-
-
-// //board(FEN_converted);
-// //prints the FEN.
-
-// // std::cout << '\n';
-// // std::cout << '\n';
-// std::cout << '\n';
-
-
-
-
+}
+ 
 
 
 
@@ -103,7 +68,6 @@ for (int i = 0; i < rows; i++){
 // // int gotocol;
 // // std::cout << "enter coordinates of where you want to move it: " << '\n';
 // // std::cin>> gotorow, gotocol;
-
 
 
 // // if(myboard[findrow][findcol] == 'P'){
@@ -131,8 +95,5 @@ for (int i = 0; i < rows; i++){
 
 
 
-return 0;
 
-}
- 
    
