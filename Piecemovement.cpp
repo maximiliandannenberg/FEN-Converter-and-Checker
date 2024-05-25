51a4a6a3274bbb6cf45345510b5a92db49ad746f
maximiliandannenberg <130485413+maximiliandannenberg@users.noreply.github.com>
2024-05-25 23:21:31 +0200

    Create Piecemovement.cpp

---
+++ Piecemovement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Piecemovement.hpp"
+
+
+
+char** PawnMoves(char** myboard){
+        std::cout << "hi I work";
+    return myboard;
+}
+
+// // std::cout << "enter coordinates of what you want to move: " << '\n';
+// // int findrow;
+// // int findcol;
+// // std::cin>> findrow, findcol;
+
+// // int gotorow;
+// // int gotocol;
+// // std::cout << "enter coordinates of where you want to move it: " << '\n';
+// // std::cin>> gotorow, gotocol;
+
+
+// // if(myboard[findrow][findcol] == 'P'){
+// //     if(myboard[findrow + 1][findcol] == myboard[gotorow][gotocol]){
+
+// //         myboard[gotorow][gotocol] = 'P';
+// //         myboard[findrow][findcol] = '0';
+// //     }
+// // //    myboard[findrow][findcol] myboard[gotorow][gotocol]
+// // }
+
+
+
+
+
+
+// // //     int findrow, findcol;
+// // // std::cout << "select what number you want to get, insert first the row, then the column." << "\n";
+
+// // // std::cin >> findrow >> findcol;
+// // // std::cout << myboard[fin drow][findcol];
+
+
+// std::cout << "\n" << FEN_checked_pieces;
+
