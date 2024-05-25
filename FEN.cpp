47e788e740ac51e5a6ec5f8f44958db69c96d609
maximiliandannenberg <130485413+maximiliandannenberg@users.noreply.github.com>
2024-05-25 23:20:34 +0200

    Update and rename Functions.cpp to FEN.cpp

--- FEN.cpp
+++ FEN.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <list>
-#include "Functions.hpp"
+#include "FEN.hpp"
 
 
 //sample FENs to use: 8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1
@@ -17,8 +17,6 @@ FEN_Games CreateFEN(std::string FEN){
     char AllowedBoardPieces[14] = {'k','K','r','R','n','N','p','P','b','B','0','q','Q'};
     char AllowedCharactersInFEN[22] = {'k','K','r','R','n','N','p','P','b','B','0','q','Q','0','1','2','3','4','5','6','7','8'};
 
-
-    
     //we are counting the spaces after the first FEN string.
     //The amount of spaces = the type of chess information (playerturn, castlingrights)
     for (int i = 0; i < FEN.length(); i++){
@@ -85,14 +83,23 @@ FEN_Games CreateFEN(std::string FEN){
                 }
         }
         
-    bool FEN_checked_pieces = FEN_Check_Piece_Count(FENvalue);
-    std::cout << "\n\n\n" << FEN_checked_pieces << "\n";
+        if (broken == false){
+        bool FEN_checked_pieces = FEN_Check_Piece_Count(FENvalue);
+        std::cout << "\n\n\n" << FEN_checked_pieces << "\n";
         if(FEN_checked_pieces == true){
             broken = 1;
         }
+        }
 
+   if (broken == true){
+        std::vector<char> ItIsBroken = {'1'};
+        FEN_Games game(ItIsBroken, broken);
+        return game;
+   }else{
     FEN_Games game(FENvalue, broken);
     return game;
+   }
+    
 }
 
 
@@ -197,8 +204,7 @@ int wKing = 0, bKing = 0, wKnights = 0, bKnights = 0, wRooks = 0, bRooks = 0, wP
     return broken;
 }
 
-
-char** boardinitiliazer(int rows, int cols){
+char** boardinitiliazer(int rows, int cols, FEN_Games game){
 
     char** myboard = new char*[rows];
 
@@ -207,11 +213,34 @@ char** boardinitiliazer(int rows, int cols){
         //at each memory address, which is a pointer to an array, you assign the length of the array.
     }
 
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                int position = (i * 8) + j;
+                myboard[i][j] = game.FEN[position];
+            }
+        }
+    
     return myboard;
 
 }
 
+ void printboard(char** myboard, int rows, int cols){
+    for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                std::cout << myboard[i][j] << " ";
+            }
+            std::cout << '\n';
+        }
+    }
 
+void deletemyboard(char** myboard, int rows, int cols){
+    for(int i = 0; i < rows; i++ ){
+        delete[] myboard[i];
+    }
+    delete[] myboard;
+    myboard = NULL;
+}
 
 
 
